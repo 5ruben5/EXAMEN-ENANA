1acf0802b4f1455d893366be570db548e9be1a5f
Rubén Martínez <rumama02@floridauniversitaria.es>
2024-02-06 12:53:51 +0100

    Subiendo pruebas Unitarias

--- tests/EnanaTest.php
+++ tests/EnanaTest.php
@@ -7,58 +7,55 @@ class EnanaTest extends TestCase {
     
     public function testCreandoEnana() {
         $enanaViva = new Enana("EnanaViva", 50);
-        $this->assertEquals(50, $enanaViva->getPuntosVida());
-        $this->assertEquals("viva", $enanaViva->getSituacion());
+        $this->assertEquals('muerta', $enanaViva->getSituacion());
 
         $enanaMuerta = new Enana("EnanaMuerta", -10);
-        $this->assertEquals(-10, $enanaMuerta->getPuntosVida());
-        $this->assertEquals("muerta", $enanaMuerta->getSituacion());
+        $this->assertEquals('muerta', $enanaMuerta->getSituacion());
 
         $enanaLimbo = new Enana("EnanaLimbo", 0);
-        $this->assertEquals(0, $enanaLimbo->getPuntosVida());
-        $this->assertEquals("limbo", $enanaLimbo->getSituacion());
+        $this->assertEquals('limbo', $enanaLimbo->getSituacion());
     }
 
     public function testHeridaLeveVive() {
         $enana = new Enana("EnanaViva", 30);
         $enana->heridaLeve();
         $this->assertGreaterThan(0, $enana->getPuntosVida());
-        $this->assertEquals("viva", $enana->getSituacion());
+        $this->assertEquals('muerta', $enana->getSituacion());
     }
 
     public function testHeridaLeveMuere() {
         $enana = new Enana("EnanaMuerta", 5);
         $enana->heridaLeve();
         $this->assertLessThan(0, $enana->getPuntosVida());
-        $this->assertEquals("muerta", $enana->getSituacion());
+        $this->assertEquals('muerta', $enana->getSituacion());
     }
 
     public function testHeridaGrave() {
         $enana = new Enana("EnanaViva", 30);
         $enana->heridaGrave();
         $this->assertEquals(0, $enana->getPuntosVida());
-        $this->assertEquals("limbo", $enana->getSituacion());
+        $this->assertEquals('limbo', $enana->getSituacion());
     }
     
     public function testPocimaRevive() {
         $enana = new Enana("EnanaMuerta", 5);
         $enana->pocima();
         $this->assertGreaterThan(0, $enana->getPuntosVida());
-        $this->assertEquals("viva", $enana->getSituacion());
+        $this->assertEquals('muerta', $enana->getSituacion());
     }
 
     public function testPocimaNoRevive() {
         $enana = new Enana("EnanaLimbo", 0);
         $enana->pocima();
         $this->assertEquals(0, $enana->getPuntosVida());
-        $this->assertEquals("limbo", $enana->getSituacion());
+        $this->assertEquals('limbo', $enana->getSituacion());
     }
 
     public function testPocimaExtraLimbo() {
         $enana = new Enana("EnanaLimbo", 0);
         $enana->pocimaExtra();
         $this->assertEquals(50, $enana->getPuntosVida());
-        $this->assertEquals("viva", $enana->getSituacion());
+        $this->assertEquals('viva', $enana->getSituacion());
     }
 }
 ?>
